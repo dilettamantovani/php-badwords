ded1ae33f2c8cfd206de361666cb5ef3cd4d378a
dilettits <mantovani.d3@gmail.com>
2023-12-18 09:36:14 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello PHP</title>
+</head>
+<body>
+    <!-- variables -->
+    <?php $greetings = "Hello PHP World!"; ?>
+
+    <h1><?php echo $greetings; ?></h1>
+    
+</body>
+</html>
